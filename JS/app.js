c236b571d5690314ff2306b837f52130ae8cd9f5
RodrigoDelgado005 <162932332+RodrigoDelgado005@users.noreply.github.com>
2024-04-11 19:53:06 -0300

    first commit

---
+++ JS/app.js
@@ -0,0 +1,5 @@
+function cambiarElEstilo() {
+    let pun = document.getElementById('Paises')
+    let punrec = document.getElementById('cambiarEstilo')
+    pun.style.listStyleType = punrec.value
+}
